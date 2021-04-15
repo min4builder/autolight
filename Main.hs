a876923d26fc3d2ed13d68e4f6e1f928f2ef4896
Lucas Ieks <lieks124@gmail.com>
2021-04-15 17:33:45 -0300

    Use standard Comonad

--- Main.hs
+++ Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import qualified Codec.Picture as Juicy
 import qualified Codec.Picture.Types as Juicy
+import Control.Comonad (Comonad, extract, extend)
 import Data.Maybe (fromJust)
 import Data.Vector (Vector, (!), generate)
 import Data.Vector.Generic (convert)
@@ -9,17 +10,6 @@ import Data.Word (Word8)
 import System.Environment (getArgs)
 
 --- HELPERS
-
-infixl 1 >=>
-
-class Functor w => Comonad w where
-    extract :: w a -> a
-    duplicate :: w a -> w (w a)
-    duplicate = extend id
-    extend :: (w a -> b) -> w a -> w b
-    extend f = fmap f . duplicate
-    (>=>) :: (w a -> b) -> (w b -> c) -> (w a -> c)
-    (>=>) a b = b . extend a
 
 class PixelAdressable i where
     pixel :: i a -> Int -> Int -> Maybe a
