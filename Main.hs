b23bc7fbdadfa7c5da883955e2d219a3294d4fb3
Lucas Ieks <lieks124@gmail.com>
2021-07-22 16:01:43 -0300

    Attempt at making Repa fast

--- Main.hs
+++ Main.hs
@@ -1,13 +1,13 @@
 {-# LANGUAGE BangPatterns, FlexibleContexts #-}
 module Main where
 
-import Control.Comonad (extend, extract)
+import Data.Array.Repa (D)
 import Data.Array.Repa.Index
 import Data.Array.Repa.Shape
-import Data.Array.Repa.Repr.Vector (toVector)
+import Data.Array.Repa.Repr.Vector --(V, toVector)
 import Data.List (foldl')
 import Data.Word (Word8)
-import MatrixComonad
+import MatrixComonad as MC
 
 import Criterion.Main
 
@@ -25,11 +25,13 @@ gaussianBlur' r = vertical . horizontal
               foldl' add 0 [ gauss (fromIntegral (y' - y)) * mindex img 0 (ix2 x y') | y' <- [y - 3*(round r) .. y + 3*(round r)] ]
           gauss n = exp (-(n**2 / (2 * r**2))) / sqrt (2 * pi * r**2)
 
-gradient :: (Matrix m DIM2, Num p) => FocusedMatrix m DIM2 p -> FocusedMatrix m DIM2 (p, p)
+gradient :: (Matrix m r DIM2 p, Matrix m (MResult m) DIM2 (p, p), Num p) =>
+            FocusedMatrix m r DIM2 p -> FocusedMatrix m (MResult m) DIM2 (p, p)
 gradient = extend $ \img ->
     let v = extract img in (index img v (ix2 1 0) - v, index img v (ix2 0 1) - v)
 
-gradient' :: (Matrix m DIM2, Num p) => m DIM2 p -> m DIM2 (p, p)
+gradient' :: (Matrix m r DIM2 p, Matrix m (MResult m) DIM2 (p, p), Num p) =>
+             m r DIM2 p -> m (MResult m) DIM2 (p, p)
 gradient' = mrun $ \img (Z :. x :. y) ->
     let v = mindex img undefined (ix2 x y) in (mindex img v (ix2 (x + 1) y) - v, mindex img v (ix2 x (y + 1)) - v)
 
@@ -41,7 +43,7 @@ distance' r = mrun $ \img (Z :. x :. y) ->
     minimum (r : [ sqrt (x'*x' + y'*y') | x' <- [(fromIntegral x) - r .. (fromIntegral x) + r], y' <- [(fromIntegral y) - r .. (fromIntegral y) + r],
         mindex img False (ix2 (round x') (round y')) ])
 
-autolight img = MatrixComonad.zipWith (*) img $ gaussianBlur 1 shadow
+autolight img = MC.zipWith (*) img $ gaussianBlur 1 shadow
     where (Z :. w :. h) = msize $ unfocus img
           blurr = minimum [fromIntegral w, fromIntegral h] / 16
           distr = blurr / 2
@@ -49,14 +51,11 @@ autolight img = MatrixComonad.zipWith (*) img $ gaussianBlur 1 shadow
           (lx, ly) = (1, 1)
 
           blurry = gaussianBlur blurr img
-          delta = fmap (\(dx, dy) -> lx*dx + ly*dy) $ gradient blurry
+          delta = MC.map (\(dx, dy) -> lx*dx + ly*dy) $ gradient blurry
 
-          dist = distance distr $ fmap (< 0.5) img
-          mdist = fmap (\v -> distw * ((1 / (1 + exp (-v * 6 / distr))) - 1)) dist
-          shadow = fmap ((+ 0.8) . (* 0.2) . signum) $ MatrixComonad.zipWith (+) delta mdist
-{-# SPECIALIZE autolight :: FocusedMatrix MatrixVector DIM2 Float -> FocusedMatrix MatrixVector DIM2 Float #-}
-{-# SPECIALIZE autolight :: FocusedMatrix MatrixArray DIM2 Float -> FocusedMatrix MatrixArray DIM2 Float #-}
-{-# SPECIALIZE autolight :: FocusedMatrix MatrixParallel DIM2 Float -> FocusedMatrix MatrixParallel DIM2 Float #-}
+          dist = distance distr $ MC.map (< 0.5) img
+          mdist = MC.map (\v -> distw * ((1 / (1 + exp (-v * 6 / distr))) - 1)) dist
+          shadow = MC.map ((+ 0.8) . (* 0.2) . signum) $ MC.zipWith (+) delta mdist
 
 autolight' img = mzipWith (*) img $ gaussianBlur' 1 shadow
     where (Z :. w :. h) = msize img
@@ -66,42 +65,33 @@ autolight' img = mzipWith (*) img $ gaussianBlur' 1 shadow
           (lx, ly) = (1, 1)
 
           blurry = gaussianBlur' blurr img
-          delta = fmap (\(dx, dy) -> lx*dx + ly*dy) $ gradient' blurry
+          delta = mmap (\(dx, dy) -> lx*dx + ly*dy) $ gradient' blurry
 
-          dist = distance' distr $ fmap (< 0.5) img
-          mdist = fmap (\v -> distw * ((1 / (1 + exp (-v * 6 / distr))) - 1)) dist
-          shadow = fmap ((+ 0.8) . (* 0.2) . signum) $ mzipWith (+) delta mdist
-{-# SPECIALIZE autolight' :: MatrixVector DIM2 Float -> MatrixVector DIM2 Float #-}
-{-# SPECIALIZE autolight' :: MatrixArray DIM2 Float -> MatrixArray DIM2 Float #-}
-{-# SPECIALIZE autolight' :: MatrixParallel DIM2 Float -> MatrixParallel DIM2 Float #-}
+          dist = distance' distr $ mmap (< 0.5) img
+          mdist = mmap (\v -> distw * ((1 / (1 + exp (-v * 6 / distr))) - 1)) dist
+          shadow = mmap ((+ 0.8) . (* 0.2) . signum) $ mzipWith (+) delta mdist
 
-gameOfLife :: Matrix m DIM2 => FocusedMatrix m DIM2 Bool -> FocusedMatrix m DIM2 Bool
+gameOfLife :: Matrix m (MResult m) DIM2 Bool => FocusedMatrix m (MResult m) DIM2 Bool -> FocusedMatrix m (MResult m) DIM2 Bool
 gameOfLife = extend $ \img ->
     let n = sum [ if index img False (ix2 x y) then 1 else 0 | x <- [-1 .. 1], y <- [-1 .. 1], (x, y) /= (0, 0) ] in
         n == 3 || (extract img && n == 2)
-{-# SPECIALIZE gameOfLife :: FocusedMatrix MatrixVector DIM2 Bool -> FocusedMatrix MatrixVector DIM2 Bool #-}
-{-# SPECIALIZE gameOfLife :: FocusedMatrix MatrixArray DIM2 Bool -> FocusedMatrix MatrixArray DIM2 Bool #-}
-{-# SPECIALIZE gameOfLife :: FocusedMatrix MatrixParallel DIM2 Bool -> FocusedMatrix MatrixParallel DIM2 Bool #-}
 
-gameOfLife' :: Matrix m DIM2 => m DIM2 Bool -> m DIM2 Bool
+gameOfLife' :: Matrix m (MResult m) DIM2 Bool => m (MResult m) DIM2 Bool -> m (MResult m) DIM2 Bool
 gameOfLife' = mrun $ \img (Z :. x :. y) ->
     let n = sum [ if mindex img False (ix2 (x + dx) (y + dy)) then 1 else 0 | dx <- [-1 .. 1], dy <- [-1 .. 1], (dx, dy) /= (0, 0) ] in
         n == 3 || (mindex img False (ix2 x y) && n == 2)
-{-# SPECIALIZE gameOfLife' :: MatrixVector DIM2 Bool -> MatrixVector DIM2 Bool #-}
-{-# SPECIALIZE gameOfLife' :: MatrixArray DIM2 Bool -> MatrixArray DIM2 Bool #-}
-{-# SPECIALIZE gameOfLife' :: MatrixParallel DIM2 Bool -> MatrixParallel DIM2 Bool #-}
 
 takeiterate 0 _ _ = []
 takeiterate !n f a = a : takeiterate (n - 1) f (f a)
 
-toB :: Matrix m DIM2 => m DIM2 Word8 -> m DIM2 Bool
-toB = fmap (> 0.5) . toF
-fromB :: Matrix m DIM2 => m DIM2 Bool -> m DIM2 Word8
-fromB = fromF . fmap (\v -> if v then 1 else 0)
-toF :: Matrix m DIM2 => m DIM2 Word8 -> m DIM2 Float
-toF = fmap ((/ 256) . fromIntegral)
-fromF :: Matrix m DIM2 => m DIM2 Float -> m DIM2 Word8
-fromF = fmap (toEnum . round . (* 255) . clamp 0 1)
+toF :: (Matrix m r sh Word8, Matrix m (MResult m) sh Float) =>
+       m r sh Word8 -> m (MResult m) sh Float
+toF = mmap ((/ 256) . fromIntegral)
+fromF :: (Matrix m r sh Float, Matrix m (MResult m) sh Word8) =>
+         m r sh Float -> m (MResult m) sh Word8
+fromF = mmap (toEnum . round . (* 255) . clamp 0 1)
+toB = mmap (> 0.5) . toF
+fromB = fromF . mmap (\v -> if v then 1 else 0)
 clamp a b c
     | c < a = a
     | c > b = b
@@ -117,7 +107,7 @@ main = do
             bench "testsmall'" $ nf (vmData . fromF . autolight') $ toF testsmall,
             bench "testbig" $ nf (vmData . fromF . unfocus . autolight) $ focus $ toF testbig,
             bench "testbig'" $ nf (vmData . fromF . autolight') $ toF testbig,
-            bench "life0" $ nf (vmData . fromB . last . map unfocus . takeiterate 16 gameOfLife) $ focus $ toB life0,
+            bench "life0" $ nf (vmData . fromB . last . Prelude.map unfocus . takeiterate 16 gameOfLife) $ focus $ toB life0,
             bench "life0'" $ nf (vmData . fromB . last . takeiterate 16 gameOfLife') $ toB life0
             ],
         bgroup "MatrixArray" [
@@ -125,16 +115,16 @@ main = do
             bench "testsmall" $ nf (vmData . fromF . fromArray . unfocus . autolight) $ focus $ toArray $ toF testsmall,
             bench "testbig" $ nf (vmData . fromF . fromArray . unfocus . autolight) $ focus $ toArray $ toF testbig,
             bench "testbig" $ nf (vmData . fromF . fromArray . autolight') $ toArray $ toF testbig,
-            bench "life0" $ nf (vmData . fromB . last . map (fromArray . unfocus) . takeiterate 16 gameOfLife) $ focus $ toArray $ toB life0,
-            bench "life0'" $ nf (vmData . fromB . last . map fromArray . takeiterate 16 gameOfLife') $ toArray $ toB life0
+            bench "life0" $ nf (vmData . fromB . last . Prelude.map (fromArray . unfocus) . takeiterate 16 gameOfLife) $ focus $ mresult $ toArray $ toB life0,
+            bench "life0'" $ nf (vmData . fromB . last . Prelude.map fromArray . takeiterate 16 gameOfLife') $ mresult $ toArray $ toB life0
             ],
         bgroup "MatrixParallel" [
             bench "testsmall" $ nf (vmData . fromF . fromParallel . unfocus . autolight) $ focus $ toParallel $ toF testsmall,
             bench "testsmall'" $ nf (vmData . fromF . fromParallel . autolight') $ toParallel $ toF testsmall,
             bench "testbig" $ nf (vmData . fromF . fromParallel . unfocus . autolight) $ focus $ toParallel $ toF testbig,
             bench "testbig'" $ nf (vmData . fromF . fromParallel . autolight') $ toParallel $ toF testbig,
-            bench "life0" $ nf (vmData . fromB . last . map (fromParallel . unfocus) . takeiterate 16 gameOfLife) $ focus $ toParallel $ toB life0,
-            bench "life0'" $ nf (vmData . fromB . last . map fromParallel . takeiterate 16 gameOfLife') $ toParallel $ toB life0
+            bench "life0" $ nf (vmData . fromB . last . Prelude.map (fromParallel . unfocus) . takeiterate 16 gameOfLife) $ focus $ mresult $ toParallel $ toB life0,
+            bench "life0'" $ nf (vmData . fromB . last . Prelude.map fromParallel . takeiterate 16 gameOfLife') $ mresult $ toParallel $ toB life0
             ]
         ]
 
--- MatrixComonad.hs
+++ MatrixComonad.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE BangPatterns, FlexibleInstances, MultiParamTypeClasses #-}
+{-# LANGUAGE BangPatterns, ConstraintKinds, FlexibleContexts, FlexibleInstances, MultiParamTypeClasses, TypeFamilies, UndecidableInstances #-}
 module MatrixComonad where
 
 import qualified Codec.Picture as Juicy
@@ -6,35 +6,48 @@ import qualified Codec.Picture.Types as Juicy
 import Control.Comonad (Comonad, extend, extract)
 import Control.Monad.Identity (runIdentity)
 import Data.Array.Repa as R
+import Data.Array.Repa.Eval (Load)
 import Data.Array.Repa.Repr.Vector
 import Data.Either (fromRight)
 import Data.Maybe (fromJust)
+import Data.Kind (Constraint)
 import Data.Vector as V (Vector, (!), generate, zipWith)
 import Data.Vector.Generic (convert)
 import Data.Word (Word8)
 
-class (Functor (m sh), Shape sh) => Matrix m sh where
-    mindex :: m sh a -> a -> sh -> a
-    minside :: m sh a -> sh -> Bool
-    mrun :: (m sh a -> sh -> b) -> m sh a -> m sh b
-    mnew :: sh -> (sh -> a) -> m sh a
-    mzipWith :: (a -> b -> c) -> m sh a -> m sh b -> m sh c
-    msize :: m sh a -> sh
+type Matrix m p sh a = (Shape sh, MatrixImpl m p sh a, MValid m p sh a,
+                        MatrixImpl m (MResult m) sh a, MValid m (MResult m) sh a,
+                        MatrixImpl m (MNormal m) sh a, MValid m (MNormal m) sh a)
+
+class MatrixImpl m p sh a where
+    type MValid m p sh a :: Constraint
+    type MValid m p sh a = MatrixImpl m p sh a
+    type MResult m :: *
+    type MResult m = ()
+    type MNormal m :: *
+    type MNormal m = ()
+    mresult :: Matrix m p sh a => m p sh a -> m (MResult m) sh a
+    mresult = mmap id
+    mindex :: Matrix m p sh a => m p sh a -> a -> sh -> a
+    minside :: Matrix m p sh a => m p sh a -> sh -> Bool
+    mrun :: Matrix m p sh a => (m (MNormal m) sh a -> sh -> b) -> m p sh a -> m (MResult m) sh b
+    mnew :: (Matrix m p sh a, p ~ MResult m) => sh -> (sh -> a) -> m p sh a
+    mmap :: (Matrix m p sh a, Matrix m (MResult m) sh b) => (a -> b) -> m p sh a -> m (MResult m) sh b
+    mzipWith :: (Matrix m p sh a, Matrix m q sh b, Matrix m (MResult m) sh c) => (a -> b -> c) -> m p sh a -> m q sh b -> m (MResult m) sh c
+    msize :: Matrix m p sh a => m p sh a -> sh
 
 --- VECTOR PROCESSING MATRIX
 
-data Shape sh => MatrixVector sh a = MatrixVector { vmShape :: !sh, vmData :: Vector a }
+data MatrixVector r sh a = MatrixVector { vmShape :: !sh, vmData :: Vector a }
 
-instance Shape sh => Functor (MatrixVector sh) where
-    fmap f (MatrixVector sh d) = MatrixVector sh $ fmap f d
-
-instance Shape sh => Matrix MatrixVector sh where
+instance MatrixImpl MatrixVector () sh a where
     mindex mat@(MatrixVector sh d) v !p
         | minside mat p = d V.! (toIndex sh p)
         | otherwise = v
     minside d !p = inShape (vmShape d) p
     mrun f d = MatrixVector (vmShape d) $ generate (size $ vmShape d) (f d . fromIndex (vmShape d))
     mnew sh f = MatrixVector sh $ generate (size sh) (f . fromIndex sh)
+    mmap f (MatrixVector sh d) = MatrixVector sh $ fmap f d
     mzipWith f (MatrixVector sha a) (MatrixVector shb b)
         | sha == shb = MatrixVector sha $ V.zipWith f a b
         | otherwise = error "Mismatching shapes"
@@ -42,53 +55,74 @@ instance Shape sh => Matrix MatrixVector sh where
 
 --- SERIAL PROCESSING MATRIX
 
-newtype Shape sh => MatrixArray sh a = MatrixArray { smData :: Array V sh a }
+class RepaEvaluator m r sh a where
+    revaluate :: Matrix m r sh a => m r sh a -> m (MNormal m) sh a
 
-toArray :: Shape sh => MatrixVector sh a -> MatrixArray sh a
+newtype MatrixArray r sh a = MatrixArray { smData :: Array r sh a }
+
+toArray :: MatrixVector () sh a -> MatrixArray V sh a
 toArray (MatrixVector sh d) = MatrixArray $ fromVector sh d
 
-fromArray :: Shape sh => MatrixArray sh a -> MatrixVector sh a
-fromArray (MatrixArray d) = MatrixVector (extent d) $ toVector d
+fromArray :: (Matrix MatrixArray r sh a, RepaEvaluator MatrixArray r sh a) =>
+             MatrixArray r sh a -> MatrixVector () sh a
+fromArray d = MatrixVector (msize d) $ toVector $ smData $ revaluate d
 
-instance Shape sh => Functor (MatrixArray sh) where
-    fmap f (MatrixArray d) = MatrixArray $ computeS $ R.map f d
+instance RepaEvaluator MatrixArray V sh a where
+    revaluate = id
 
-instance Shape sh => Matrix MatrixArray sh where
+instance RepaEvaluator MatrixArray D sh a where
+    revaluate = MatrixArray . computeS . smData
+
+instance RepaEvaluator MatrixArray r sh a => MatrixImpl MatrixArray r sh a where
+    type MValid MatrixArray r sh a = Source r a
+    type MResult MatrixArray = D
+    type MNormal MatrixArray = V
+    mresult (MatrixArray d) = MatrixArray $ delay d
     mindex mat@(MatrixArray d) v !p
         | minside mat p = d R.! p
         | otherwise = v
     minside (MatrixArray d) !p = inShape (extent d) p
-    mrun f d = MatrixArray $ computeS $ fromFunction (msize d) $ f d
-    mnew sh f = MatrixArray $ computeS $ fromFunction sh f
-    mzipWith f (MatrixArray a) (MatrixArray b) = MatrixArray $ computeS $ R.zipWith f a b
+    mrun f d = MatrixArray $ fromFunction (msize d) $ f $ revaluate d
+    mmap f (MatrixArray d) = MatrixArray $ R.map f d
+    mnew sh f = MatrixArray $ fromFunction sh f
+    mzipWith f (MatrixArray a) (MatrixArray b) = MatrixArray $ R.zipWith f a b
     msize (MatrixArray d) = extent d
 
 --- PARALLEL PROCESSING MATRIX
 
-newtype Shape sh => MatrixParallel sh a = MatrixParallel { pmData :: Array V sh a }
+newtype Shape sh => MatrixParallel r sh a = MatrixParallel { pmData :: Array r sh a }
 
-toParallel :: Shape sh => MatrixVector sh a -> MatrixParallel sh a
+instance RepaEvaluator MatrixParallel V sh a where
+    revaluate = id
+
+instance RepaEvaluator MatrixParallel D sh a where
+    revaluate = MatrixParallel . runIdentity . computeP . pmData
+
+toParallel :: Shape sh => MatrixVector () sh a -> MatrixParallel V sh a
 toParallel (MatrixVector sh d) = MatrixParallel $ fromVector sh d
 
-fromParallel :: Shape sh => MatrixParallel sh a -> MatrixVector sh a
-fromParallel (MatrixParallel d) = MatrixVector (extent d) $ toVector d
+fromParallel :: (Matrix MatrixParallel r sh a, RepaEvaluator MatrixParallel r sh a) =>
+                MatrixParallel r sh a -> MatrixVector () sh a
+fromParallel d = MatrixVector (msize d) $ toVector $ pmData $ revaluate d
 
-instance Shape sh => Functor (MatrixParallel sh) where
-    fmap f (MatrixParallel d) = MatrixParallel $ runIdentity $ computeP $ R.map f d
-
-instance Shape sh => Matrix MatrixParallel sh where
+instance RepaEvaluator MatrixParallel r sh a => MatrixImpl MatrixParallel r sh a where
+    type MValid MatrixParallel r sh a = Source r a
+    type MResult MatrixParallel = D
+    type MNormal MatrixParallel = V
+    mresult (MatrixParallel d) = MatrixParallel $ delay d
     mindex mat@(MatrixParallel d) v !p
         | minside mat p = d R.! p
         | otherwise = v
     minside (MatrixParallel d) !p = inShape (extent d) p
-    mrun f d = MatrixParallel $ runIdentity $ computeP $ fromFunction (msize d) $ f d
-    mnew sh f = MatrixParallel $ runIdentity $ computeP $ fromFunction sh f
-    mzipWith f (MatrixParallel a) (MatrixParallel b) = MatrixParallel $ runIdentity $ computeP $ R.zipWith f a b
+    mrun f d = MatrixParallel $ fromFunction (msize d) $ f $ revaluate d
+    mnew sh f = MatrixParallel $ fromFunction sh f
+    mmap f (MatrixParallel d) = MatrixParallel $ R.map f d
+    mzipWith f (MatrixParallel a) (MatrixParallel b) = MatrixParallel $ R.zipWith f a b
     msize (MatrixParallel d) = extent d
 
 --- IMAGE HANDLING
 
-readImage :: FilePath -> IO (MatrixVector DIM2 Word8)
+readImage :: FilePath -> IO (MatrixVector () DIM2 Word8)
 readImage filePath = do
     imageR <- Juicy.readImage filePath
     case imageR of
@@ -100,13 +134,13 @@ readImage filePath = do
                         $ Juicy.convertRGB8 img
         Left err -> error $ "readMatrix: could not load image: " Prelude.++ err
 
-writePng :: FilePath -> MatrixVector DIM2 Word8 -> IO ()
+writePng :: FilePath -> MatrixVector () DIM2 Word8 -> IO ()
 writePng filePath d =
         Juicy.writePng filePath
             (Juicy.Image w h (convert $ vmData d) :: Juicy.Image Juicy.Pixel8)
     where (Z :. w :. h) = msize d
 
-writeGifAnim :: FilePath -> [MatrixVector DIM2 Word8] -> IO ()
+writeGifAnim :: FilePath -> [MatrixVector () DIM2 Word8] -> IO ()
 writeGifAnim filePath imgs = fromRight undefined $
     Juicy.writeGifAnimation filePath 10 Juicy.LoopingNever $ Prelude.map (\d ->
         let (Z :. w :. h) = msize d in
@@ -114,37 +148,36 @@ writeGifAnim filePath imgs = fromRight undefined $
 
 --- IMAGE COMONAD
 
-data (Matrix m sh, Shape sh) => FocusedMatrix m sh a = FocusedMatrix {
-    unfocus :: !(m sh a),
+data Matrix m r sh a => FocusedMatrix m r sh a = FocusedMatrix {
+    unfocus :: !(m r sh a),
     focusCoordinates :: !sh }
 
-focus :: (Matrix m sh, Shape sh) => m sh a -> FocusedMatrix m sh a
+focus :: Matrix m r sh a => m r sh a -> FocusedMatrix m r sh a
 focus mat
     | msize mat /= zeroDim = FocusedMatrix mat zeroDim
     | otherwise = error "Cannot focus empty images"
 
-zipWith :: (Matrix m sh, Shape sh) => (a -> b -> c) -> FocusedMatrix m sh a -> FocusedMatrix m sh b -> FocusedMatrix m sh c
+zipWith :: (Matrix m r sh a, Matrix m s sh b, Matrix m (MResult m) sh c) =>
+           (a -> b -> c) -> FocusedMatrix m r sh a -> FocusedMatrix m s sh b -> FocusedMatrix m (MResult m) sh c
 zipWith f (FocusedMatrix a _) (FocusedMatrix b c) =
     FocusedMatrix (mzipWith f a b) c
 
-instance (Matrix m sh, Shape sh) => Functor (FocusedMatrix m sh) where
-    fmap f (FocusedMatrix mat c) = FocusedMatrix (fmap f mat) c
+map :: (Matrix m r sh a, Matrix m (MResult m) sh b) =>
+       (a -> b) -> FocusedMatrix m r sh a -> FocusedMatrix m (MResult m) sh b
+map f (FocusedMatrix mat c) = FocusedMatrix (mmap f mat) c
 
-instance (Matrix m sh, Shape sh) => Comonad (FocusedMatrix m sh) where
-    extract (FocusedMatrix mat p) = mindex mat undefined p
-    extend f (FocusedMatrix mat p) = FocusedMatrix
-        (mrun (\mat p -> f $ FocusedMatrix mat p) mat)
-        p
+extract :: Matrix m r sh a => FocusedMatrix m r sh a -> a
+extract (FocusedMatrix mat p) = mindex mat undefined p
 
-index :: (Matrix m sh, Shape sh) => FocusedMatrix m sh a -> a -> sh -> a
+extend :: (Matrix m r sh a, Matrix m (MNormal m) sh a, Matrix m (MResult m) sh b) =>
+          (FocusedMatrix m (MNormal m) sh a -> b) -> FocusedMatrix m r sh a -> FocusedMatrix m (MResult m) sh b
+extend f (FocusedMatrix mat p) = FocusedMatrix
+    (mrun (\mat p -> f $ FocusedMatrix mat p) mat)
+    p
+
+index :: Matrix m r sh a => FocusedMatrix m r sh a -> a -> sh -> a
 index (FocusedMatrix mat fp) d p = mindex mat d $ addDim fp p
-{-# SPECIALIZE MatrixComonad.index :: Shape sh => FocusedMatrix MatrixVector sh a -> a -> sh -> a #-}
-{-# SPECIALIZE MatrixComonad.index :: Shape sh => FocusedMatrix MatrixArray sh a -> a -> sh -> a #-}
-{-# SPECIALIZE MatrixComonad.index :: Shape sh => FocusedMatrix MatrixParallel sh a -> a -> sh -> a #-}
 
-inside :: (Matrix m sh, Shape sh) => FocusedMatrix m sh a -> sh -> Bool
+inside :: (Matrix m r sh a, Shape sh) => FocusedMatrix m r sh a -> sh -> Bool
 inside (FocusedMatrix mat fp) p = minside mat $ addDim fp p
-{-# SPECIALIZE inside :: Shape sh => FocusedMatrix MatrixVector sh a -> sh -> Bool #-}
-{-# SPECIALIZE inside :: Shape sh => FocusedMatrix MatrixArray sh a -> sh -> Bool #-}
-{-# SPECIALIZE inside :: Shape sh => FocusedMatrix MatrixParallel sh a -> sh -> Bool #-}
 
